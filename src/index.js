e92958cd546d1abbfb9dbac53c586e0dd04efd84
Ivan <ivan.abt69@outlook.com>
2021-04-23 16:01:56 -0400

    Todo app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import "./style.css"
+import ToDoList from "./toDoList.js"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/toDoItemt.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class toDoItemt extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTask = this.createTask.bind(this);
+    }
+    delete(key) {
+        this.props.delete(key);
+    }
+    createTask(item) {
+        return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+    }
+    render() {
+        var todoEntries = this.props.entries;
+        var listItens = todoEntries.map(this.createTask);
+        return(
+            <ul className="theList">
+                {listItens}
+            </ul>
+        )
+    }
+}
+
+export default toDoItemt;
